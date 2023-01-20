8f706634dd1a3123a79b3e206d22577a70178b0e
Gokul Prasad <gokulprasad125@gmail.com>
2023-01-19 17:41:58 -0800

    added files

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from models import Text
+
+from summarizer.summary import *
 
 # Create your views here.
 @api_view(["POST", "GET"])
 def summarize(request):
 
     if request.method == "POST":
-        return None
+
+        text = request.data["text"]
+
+        new_addition = Text(text = text)
+        new_addition.save()
+
+        return Response(status = 200)
+
+    else:
+
+        latest = Text.objects.latest("time_sent")
+        summary = summarize(latest)
+
+        return summary
