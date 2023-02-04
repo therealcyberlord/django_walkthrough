cbc2d35232619a0db23f6a0c43aa4cdbab30920a
Gokul Prasad <gokulprasad125@gmail.com>
2023-02-04 12:11:01 -0800

    edited Docker

--- my_project/my_app/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-class TextSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Text
-        fields = ["text", "time_sent"]
