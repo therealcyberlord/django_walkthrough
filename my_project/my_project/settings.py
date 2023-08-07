383fdda8761b7f97aecb18c25cdc9f520c26c8c7
Xingyu Bian <xingyubian@umass.edu>
2023-08-06 21:27:34 -0400

    moved dockerfile

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get("DEBUG") else False
 
 ALLOWED_HOSTS = ["*"]
 
